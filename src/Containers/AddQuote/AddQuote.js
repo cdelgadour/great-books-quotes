c43b326fb41fb6e4cf9a6ad94dbffca654b89d41
Carlos Delgado <cd.urbaez@gmail.com>
2019-08-17 21:45:31 -0400

    Created form with formik library

--- src/Containers/AddQuote/AddQuote.js
+++ src/Containers/AddQuote/AddQuote.js
@@ -1,60 +1,58 @@
-import React, { Component } from 'react';
-import BasicForm from '../../Components/Form/Form'
+import React from 'react';
 import './AddQuote.scss'
+import { withFormik, Form, Field } from 'formik'
+import * as Yup from 'yup'
+import axios from "../../axios-add";
 
-class addQuote extends Component {
-    state = {
-        author: '',
-        bookTitle: '',
-        text: ''
-    };
+const addQuoteForm = ({values,
+                      errors,
+                      touched,
+                          status,
+                      isSubmitting}) => {
+    return <Form className="addQuoteForm">
+        <h1>Add a quote</h1>
+        {status && touched.length !== undefined ? <h2>{status.formStatus}</h2> : null}
+        <div className="inputContainer">
+            <Field type="text" name="bookTitle" placeholder="Name of the book"/>
+            {touched.bookTitle && errors.bookTitle? <p className="formError">{errors.bookTitle}</p> : null}
+        </div>
+        <div className="inputContainer">
+            <Field type="text" name="bookAuthor" placeholder="Name of the author"/>
+            {touched.bookAuthor && errors.bookAuthor ? <p className="formError">{errors.bookAuthor}</p> : null}
+        </div>
+        <div className="inputContainer">
+            <Field type="text" component="textarea" name="bookQuote" placeholder="Please write the quote..."/>
+            {touched.bookQuote && errors.bookQuote ? <p className="formError">{errors.bookQuote}</p> : null}
+        </div>
+        <button type="submit" disabled={isSubmitting}>Add Quote</button>
+        <div className="status">
+            {status ? <h2>{status.formStatus}</h2> : null}
+            {errors ? <h2>{errors.formError}</h2> : null}
+        </div>
+    </Form>
+};
 
-    formStateUpdate = e => {
-        this.setState({[e.target.name]:e.target.value});
-    };
+const addQuote = withFormik({
+    validationSchema: Yup.object().shape({
+        bookTitle: Yup.string().required("This is a required field"),
+        bookAuthor: Yup.string().required("This is a required field"),
+        bookQuote: Yup.string().required("This is a required field")
+    }),
 
-    sendForm = e => {
-        e.preventDefault();
-        let newData = {...this.state};
-        this.props.updateQuotes(newData);
-        this.props.history.replace('/');
-    };
-
-    cancelHandler = (e) => {
-        this.props.history.goBack();
-        e.preventDefault();
-    };
-    render() {
-        return (<div style={{textAlign: 'center', color: '#58442d'}}>
-            <BasicForm/>
-            <h2>Add a book quote!</h2>
-            <form action="" className="form">
-                <div>
-                    <input type="text"
-                           name="bookTitle"
-                           value={this.state.bookTitle}
-                           onChange={this.formStateUpdate}
-                           placeholder="Book name"/>
-                </div>
-                <div>
-                    <input type="text"
-                    name="author"
-                    value={this.state.author}
-                    onChange={this.formStateUpdate}
-                    placeholder="Book author"/></div>
-                <div>
-                    <textarea style={{width: '75%', height:'100px'}}
-                              type="text"
-                              name="text"
-                              value={this.state.text}
-                              onChange={this.formStateUpdate}
-                              placeholder="Add a quote"/>
-                </div>
-                <button onClick={this.sendForm}>Add!</button>
-                <button onClick={this.cancelHandler}>Back</button>
-            </form>
-        </div>)
+    handleSubmit(values, {props, resetForm, setErrors, setStatus, setSubmitting}) {
+        const quoteData = {author: values.bookAuthor, bookTitle: values.bookTitle, text: values.bookQuote};
+        axios.post('/authors/-LaXx5hXMY-QQ9yn9rSv.jon', quoteData)
+            .then(() => {
+                setStatus({formStatus: "Quote added!"});
+                setTimeout(() => resetForm(), 2000);
+            })
+            .catch(e => {
+                console.log(e);
+                setErrors({formError: "An error has ocurred :("});
+                setTimeout(() => resetForm(), 2000);
+                });
     }
-}
+
+})(addQuoteForm);
 
 export default addQuote;
