0be2afef39cab00df323aba55120b8d4bcc3b26b
Carlos Delgado <cd.urbaez@gmail.com>
2019-08-17 23:06:14 -0400

    Updates

--- src/Components/AnswerArea/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './Button.module.css'
-
-const AnswerButton = props => {
-    return (<button className={classes.Button} onClick={() => props.clicked(props.bookTitle)}>{props.bookTitle}</button>)
-};
-
-export default AnswerButton;
--- src/axios-add.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'https://great-books-5f716.firebaseio.com/'
-});
-
-export default instance;
